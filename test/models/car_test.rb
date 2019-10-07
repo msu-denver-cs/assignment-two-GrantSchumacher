15613392c721e54668a87aba6b7dbcc910baab82
Grant Schumacher <grantschu99@gmail.com>
2019-10-06 19:29:28 -0600

    added unit test to ensure car attributes aren't empty

--- test/models/car_test.rb
+++ test/models/car_test.rb
@@ -4,4 +4,13 @@ class CarTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "car attributes must not be empty" do
+  	car = Car.new 
+  	assert car.invalid?
+  	assert car.errors[:make].any?
+  	assert car.errors[:model].any?
+  	assert car.errors[:year].any?
+  	assert car.errors[:parts].any?
+  end
+
 end
