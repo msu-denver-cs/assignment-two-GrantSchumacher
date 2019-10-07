f7eda7baf2f578c4cfa9871b716a908fa800ccf9
Grant Schumacher <grantschu99@gmail.com>
2019-10-06 19:57:13 -0600

    Added test to for empty part name

--- test/models/part_test.rb
+++ test/models/part_test.rb
@@ -4,4 +4,10 @@ class PartTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+   test "part name must not be empty" do
+  	part = Part.new 
+  	assert part.invalid?
+  	assert part.errors[:name].any?
+  	
+  end
 end
